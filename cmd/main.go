a7b309cb7470cbd527b9950790158bd37298a997
Alexsander da Silva <alexsans@outlook.com.br>
2024-11-03 18:16:18 -0300

    feat: create route to create product

--- cmd/main.go
+++ cmd/main.go
@@ -32,6 +32,7 @@ func main() {
 	})
 	
 	server.GET("/products", productController.GetProducts)
+	server.POST("/product", productController.CreateProduct)
 
 	server.Run(":8000")
 
--- repository/product_repository.go
+++ repository/product_repository.go
@@ -45,3 +45,21 @@ func (pr *ProductRepository) GetProducts() ([]model.Product, error) {
 
 	return productList, nil
 }
+
+func (pr *ProductRepository) CreateProduct(prodcut model.Product) (int, error) {
+	var id int
+	query, err := pr.connection.Prepare("INSERT INTO product (product_name, price) VALUES($1, $2) RETURNING id")
+	if err != nil {
+		fmt.Println(err)
+		return 0, err
+	}
+
+	err = query.QueryRow(prodcut.Name, prodcut.Price).Scan(&id)
+	if err != nil {
+		fmt.Println(err)
+		return 0, err
+	}
+
+	query.Close()
+	return id, nil
+}
